b4077bd4eafc0bdeac7bfd5e9985412bdc736c52
Ishan <123245475+ishan-vg@users.noreply.github.com>
2024-01-16 22:37:40 +0530

    made the code smaller and more effieicent
    made the code smaller and more efficient by declaring a user defined function to store the main logic of the code in.

--- numguesser.py
+++ numguesser.py
@@ -2,9 +2,10 @@ import random
 
 x = random.randint(1,100) #generating a random number to guess
 count = 0
+limit = 0
 level = int(input('Choose a level from 1-3: '))
-if level == 1:
-  while count<=30: #providing an upper limit of 30 guesses 
+def game(count,limit):#Declaring a function
+  while count<=limit: #providing an upper limit of 30 guesses 
     guess = int(input('Enter your guess:\t'))
     if guess!=x:
       if guess>x:
@@ -22,43 +23,11 @@ if level == 1:
       break #ending the while loop for when the user guesses correctly before exhausting the limit of 30 guesses
   else:
     print('You have exhausted all your guesses, please try again!')
+if level == 1:
+  game(0,30)#providing a limit of 30 guesses 
 elif level == 2:
-  while count<=15:  
-    guess = int(input('Enter your guess:\t'))
-    if guess!=x:
-      if guess>x:
-        print('Wrong guess, go lower')
-        count+=1 
-        print(f'Count = {count}') 
-      elif guess<x:
-        print('Wrong guess, go higher')
-        count+=1
-        print(f'Count = {count}')
-    else:
-      print('Correct guess')
-      count+=1
-      print(f'Count = {count}')
-      break
-  else:
-    print('You have exhausted all your guesses, please try again!')
+ game(0,15)#providing a limit of 15 guesses 
 elif level == 3:
-  while count <= 7:  
-    guess = int(input('Enter your guess:\t'))
-    if guess!=x:
-      if guess>x:
-        print('Wrong guess, go lower')
-        count+=1 
-        print(f'Count = {count}') 
-      elif guess<x:
-        print('Wrong guess, go higher')
-        count+=1
-        print(f'Count = {count}')
-    else:
-      print('Correct guess')
-      count+=1
-      print(f'Count = {count}')
-      break
-  else:
-    print('You have exhausted all your guesses, please try again!')
+  game(0,7)##providing a limit of 7 guesses
 else:
   print('Invalid Level! Please enter a level from 1-3')
